bbeecde67e4baaf4c3c197c5764e59123fbe329e
leonardo-amaral <leozero103@gmail.com>
2024-02-08 18:54:48 -0300

    feat: add GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
-import { AuthProvider } from "./context/AuthContext";
-import { Ios } from "./pages/ios";
-import { IosAirplane } from "./pages/ios/airplane";
-import { Qtg } from "./pages/qtg";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import { AuthProvider } from './context/AuthContext'
+import { TemplateProvider } from './context/TemplateContext'
+import { Ios } from './pages/ios'
+import { IosAirplane } from './pages/ios/airplane'
+import { Qtg } from './pages/qtg'
+import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="" element={<Layout />}>
-              <Route path="ios/airplane" element={<IosAirplane />} />
-              <Route path="ios" element={<Ios />} />
+      <TemplateProvider>
+        <GlobalStyles />
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="" element={<Layout />}>
+                <Route path="ios/airplane" element={<IosAirplane />} />
+                <Route path="ios" element={<Ios />} />
 
-              <Route path="qtg" element={<Qtg />} />
-            </Route>
+                <Route path="qtg" element={<Qtg />} />
+              </Route>
 
-            <Route path="*" element={<Navigate to="/ios" />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+              <Route path="*" element={<Navigate to="/ios" />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
+      </TemplateProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
