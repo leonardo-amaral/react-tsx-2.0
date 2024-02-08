2ef5cedcfafbd3c30cd7b6824973e105c4671875
leonardo-amaral <leozero103@gmail.com>
2024-02-08 18:55:24 -0300

    feat: change dir

---
+++ src/types/template/colors.types.ts
@@ -0,0 +1,20 @@
+export type TColors = {
+  primary: {
+    id: string
+    name: string
+    color: string
+    updatedAt: string
+  }
+  secondary: {
+    id: string
+    name: string
+    color: string
+    updatedAt: string
+  }
+  tertiary: {
+    id: string
+    name: string
+    color: string
+    updatedAt: string
+  }
+}
